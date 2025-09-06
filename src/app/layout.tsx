1ceeb728ccfa0420cd0ae443be922a95d2ff99b8
bluemonster2025 <admin@bluemonster.com.br>
2025-09-06 17:18:25 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
+      <head>
+        <meta name="robots" content="noindex" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
